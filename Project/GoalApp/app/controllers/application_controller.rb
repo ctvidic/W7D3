fd87cbd11a2ca752a9f9275c566dc1a56d33533a
mordes89 <mordes89@gmail.com>
2021-07-28 15:05:36 -0400

    dfkis

--- Project/GoalApp/app/controllers/application_controller.rb
+++ Project/GoalApp/app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+   # - C - `#current_user`
+   # - E - `#ensure_logged_in`
+   # - L - `#login!(user)`
+   # - L - `#logout!`
+   # - L - `#logged_in?
+
+   def current_user
+      @current_user ||= User.find_by(session_token: session[:session_token])
+   end
+
+   def ensure_logged_in
+      redirect_to new_session_url unless logged_in?
+   end
+
+   def login!(user)
+      session[:session_token] = user.reset_session_token!
+   end
+
+   def logout!
+      current_user.reset_session_token! if logged_in?
+      session[:session_token] = nil
+      @current_user = nil
+   end
+
+   def logged_in?
+      !!current_user
+   end
+
+
 end
--- Project/GoalApp/app/models/user.rb
+++ Project/GoalApp/app/models/user.rb
@@ -1,6 +1,48 @@
 class User < ApplicationRecord
     validates :username, :session_token, uniqueness: true
     validates :username, :session_token, :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    # FGvPER
+
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credentials(password, username)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+
 
 
     
